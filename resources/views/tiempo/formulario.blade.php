a00d425f380f90870c91dc0b027352b1a316ca5e
Marta Pastor <91971757+MartaPS4@users.noreply.github.com>
2022-03-06 18:34:05 +0100

    estilo pagina formulario

--- resources/views/tiempo/formulario.blade.php
+++ resources/views/tiempo/formulario.blade.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <link href="https://allfont.es/allfont.css?fonts=muli" rel="stylesheet" type="text/css" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <title>ThatzWeather</title>
 </head>
 <body>
     <div id="container">
-        <p>Entérate del tiempo en la zona exacta que te interesa buscando por código postal.</p>
+        <div id="logo">
+            <img src="img/desk/d-logo.png" alt="Logo">
+        </div>
         <!-- Introducir CP y enviar -->
+        <div id="formularioCp">
+            <p>Entérate del tiempo en la zona exacta que te interesa <br/> buscando por código postal.</p>
             <form action="{{ action('myController@recibir') }}" method="POST">
                 {{ csrf_field() }}
-                <div id="formularioCp">
                     <input type="number" name="cp" placeholder="Introduce el código postal" /><br />
                     <input type="submit"value="Buscar"/>
-                </div>
             </form>
+        </div>
         <!-- Introducir CP y enviar -->
-        <p>¡Que la lluvia no te pare!</p>
+        <p id="fin">¡Que la lluvia no te pare!</p>
     </div>
 </body>
 </html>
